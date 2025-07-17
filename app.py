91b911ffe199e926a87710e5174a64a64f08bc96
Brent R. Yates <brentRyates@gmail.com>
2025-07-17 15:33:15 -0400

    Use yaml, minor tweaks

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, send_file, send_from_directory
-import json
-import random
-import os
+import yaml
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('resume.html')
+    with open('data/yates.yml', 'r') as file:
+        html_vars = yaml.safe_load(file)
+    print(html_vars)  # Debugging line to check the loaded data
+    return render_template('resume.html', **html_vars)
